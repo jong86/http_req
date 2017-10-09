5b1691fa6c775f003934ba2b15598c7399fbc6df
Jon Gaspar <jon_gaspar@hotmail.com>
2017-10-09 19:33:18 +0000

    Added step 4: function includes callback function

---
+++ step4.js
@@ -0,0 +1,32 @@
+var https = require("https");
+
+function getHTML(options, callback) {
+  
+  https.get(options, function (response) {
+    
+    var buffer = "";
+    
+    response.setEncoding("utf8");
+    
+    response.on("data", function(data) {
+      buffer += data;
+    });
+    
+    response.on("end", function() {
+      callback(buffer);
+    });
+    
+  });
+  
+}
+
+function printHTML(html) {
+  console.log("ALL DATA:\n", html);
+}
+
+var requestOptions = {
+  host: "sytantris.github.io",
+  path: "/http-examples/step2.html"
+};
+
+getHTML(requestOptions, printHTML);
